09657e7c7d131b1280baf4d9d98cfd8a7a8d6a0d
Grace <softwarewitch@gmail.com>
2024-12-10 19:11:08 -0500

    chore: aoc day 1 solution

---
+++ day1/day1.py
@@ -0,0 +1,25 @@
+total_distance = 0
+left_values, right_values = [], []
+
+with open('day1/input.txt', 'r') as io:
+
+    text = io.read().strip().split('\n')
+
+    for row in text:
+        pairs = row.split('   ') # [12823, 12823]
+        left_values.append(pairs[0])
+        right_values.append(pairs[1])
+
+    left_values.sort()
+    right_values.sort()
+
+    left_len, right_len = len(left_values), len(right_values)
+    if left_len != right_len: raise Exception('Missing pairs')
+
+
+    for i in range(left_len):
+        left_value, right_value = int(left_values[i]), int(right_values[i])
+        distance = abs(left_value - right_value)
+        total_distance += distance
+
+    print(total_distance)
